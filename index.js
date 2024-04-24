f184d617f1e4f7fbbfb1345538229d1529a055f4
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-04-24 08:38:28 -0700

    user tweet setup

--- index.js
+++ index.js
@@ -56,4 +56,9 @@ const tweetsData = [
     },     
 ]
 
-console.log(tweetsData)
+const tweetInput = document.getElementById("tweet-input")
+const tweetBtn = document.getElementById("tweet-btn")
+
+tweetBtn.addEventListener("click", () => {
+    console.log(tweetInput.value)
+})
