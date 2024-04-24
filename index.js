12fac181284d51d42c4213e62ae5fd7a4fe9912a
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-04-24 07:54:15 -0700

    revert | live server error with seperate data file

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-import { tweetsData } from "./data";
+const tweetsData = [   
+    {
+        handle: `@Hauntedblob`,
+        profilePic: `assets/ghostprofile.webp`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `When you're all set to haunt someone but they're watching a horror movie and cheering for the ghost. #Awkward #RelatableGhostMoments"`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@FullMoonGuy`,
+        profilePic: `assets/werewolfprofile.webp`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `Just tried to howl at the moon, but sneezed halfway through. Now the local wolves think I'm starting a new call. #WerewolfWoes`,
+        replies: [
+                  {
+                handle: `@BldSkr1654`,
+                profilePic: `assets/vampireprofile.webp`,
+                tweetText: `Better than waking up with a sunburn because you forgot the curtains. Count your blessings`,
+            },
+                  {
+                handle: `@Spellchick`,
+                profilePic: `assets/witchprofile.webp`,
+                tweetText: `Could be worse! Last time I sneezed during a spell, I turned my cat into a cactus. `,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@GraveyardGourmet`,
+        profilePic: `assets/zombieprofile.webp`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `Lost an arm again while reaching for snacks. If anyone finds it, please return. It's the one holding a sandwich.`,
+        replies: [
+            {
+                handle: `@Jack`,
+                profilePic: `assets/pumpkinprofile.webp`,
+                tweetText: `Heads up, if it knocks on my lid, I'm keeping the sandwich! 🎃`,
+            },
+            {
+                handle: `@EchoNightFlyer`,
+                profilePic: `assets/batprofile.webp`,
+                tweetText: `Flew by it hanging from a tree branch. Thought it was a new type of fruit! I'll guide it back to you tonight`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
+
+console.log(tweetsData)
